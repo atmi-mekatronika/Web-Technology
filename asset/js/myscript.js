1acbcc0fbff973faefcaf4c415d488fc1b3dfc8f
ysugiarto <yoh.sugiarto@gmail.com>
2024-08-16 08:49:12 +0700

    Update services

---
+++ asset/js/myscript.js
@@ -0,0 +1,9 @@
+
+function testFunction() {
+    window.alert("test");
+        console.log("test console")
+        var i = 10
+        console.log(i*12)
+        i = "Test variable"
+        console.log(i)
+}
